7db6f5c020f0ef3b5e6a4c90ada4a5edaac92592
Matthew Karle <mkarle2178@gmail.com>
2022-01-16 14:33:51 -0500

    built archive infrastructure
    added components and styles to create project gallery linking to
    documents

---
+++ src/components/home/archive/data.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+export const PROJECTS = [
+  {
+    title: "Monthly",
+    time: "Sep. 2020 - Aug. 2021",
+    to: "/coming-soon",
+    description:
+      "I took a year off from school in 2020 to work full time at Monthly. In my time there I grew so much as an engineer and got to work on loads of cool projects. Read about it here!",
+    Image: () => (
+      <StaticImage
+        src="../../../images/monthly-project.png"
+        alt="Screenshot of Monthly website"
+      />
+    ),
+  },
+  {
+    title: "Project Jeff",
+    time: "Sep. 2021 - Nov. 2021",
+    to: "/coming-soon",
+    description: `My designer at Monthly and good friend Jeff hired me as a solo contractor to implement his portfolio site. Though reasonable in scope as a static site, there were plenty of challenges to overcome between rich interactions, responsive design, and integrating with a CMS to alleviate the technical burden of future updates. More of my thoughts here.`,
+    Image: () => (
+      <StaticImage
+        src="../../../images/project-jeff.png"
+        alt="Screenshot of Jeff's portfolio"
+      />
+    ),
+  },
+  {
+    title: "Schoolwork",
+    time: "Sep. 2019 - Present",
+    to: "/coming-soon",
+    description: `I love my classes at school! Most of what I’ve done so far has tended to be more theoretical (which is fine by me, I like a mix!), but once in a while things get a bit more applied. Ranging from mobile apps to neural architecture search research, here are some fun projects I had a hand in during my studies so far.`,
+    Image: () => (
+      <StaticImage
+        src="../../../images/bigger-and-faster.png"
+        alt="Screenshot of NAS research paper"
+      />
+    ),
+  },
+];
--- src/components/home/archive/index.js
+++ src/components/home/archive/index.js
@@ -1,15 +1,38 @@
 import * as styles from "./archive.module.scss";
 
 import React from "react";
+import { Link } from "gatsby";
 import SnapScroller from "components/basics/snap-scroller";
 import mergeDefaults from "utils/merge-defaults";
+import MD from "components/basics/md";
+import { PROJECTS } from "./data";
 
 const Archive = (props) => (
   <SnapScroller.Panel
     {...mergeDefaults({ className: styles.archive }, props)}
     id="archive"
   >
-    World
+    <div className={styles.header}>Some things I’ve done</div>
+    <div className={styles.subheader}>
+      Here’s a little select work and project experiences from recent years that
+      I’ve written a bit about.
+    </div>
+    {PROJECTS.map(({ title, description, Image, time, to }, idx) => (
+      <Link
+        key={`${title}-${idx}`}
+        className={styles.row + (idx % 2 == 1 ? ` ${styles.rev}` : "")}
+        to={to}
+      >
+        <div className={styles.imageContainer}>
+          <Image />
+        </div>
+        <div className={styles.textContainer}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.time}>{time}</div>
+          <MD className={styles.description}>{description}</MD>
+        </div>
+      </Link>
+    ))}
   </SnapScroller.Panel>
 );
 
