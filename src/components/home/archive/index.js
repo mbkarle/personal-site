8b2d9664182b66fe580c71d824eb2d560c7b219b
Matthew Karle <mkarle2178@gmail.com>
2022-01-16 01:09:04 -0500

    added id to archive for url scroll

--- src/components/home/archive/index.js
+++ src/components/home/archive/index.js
@@ -5,7 +5,10 @@ import SnapScroller from "components/basics/snap-scroller";
 import mergeDefaults from "utils/merge-defaults";
 
 const Archive = (props) => (
-  <SnapScroller.Panel {...mergeDefaults({ className: styles.archive }, props)}>
+  <SnapScroller.Panel
+    {...mergeDefaults({ className: styles.archive }, props)}
+    id="archive"
+  >
     World
   </SnapScroller.Panel>
 );
