28a9821d221a93dd1f007b6ddcb754f1e403c266
Matthew Karle <mkarle2178@gmail.com>
2022-01-17 23:10:40 -0500

    update text playback hook to move backwards
    Also added composing hook to move through sequence of text playbacks

--- src/hooks/text-playback.js
+++ src/hooks/text-playback.js
@@ -1,25 +1,47 @@
 import { useEffect, useState } from "react";
 
-const useTextPlayback = (text, interval) => {
-  const [nextIndex, setNextIndex] = useState(0);
+const getTextEdit = (current, target) => {
+  // if target has changed such that current is not a prefix, start removing current
+  if (current.length > target.length || !target.startsWith(current)) {
+    return current.substring(0, current.length - 1);
+  }
+  // otherwise if current is a prefix of target, build towards target
+  return target.substring(0, current.length + 1);
+};
+
+const useTextPlayback = (text = "", interval = 80) => {
   const [displayText, setDisplayText] = useState("");
 
   useEffect(() => {
-    const distFromEnd = text?.length - nextIndex;
-    if (distFromEnd >= 0) {
-      setDisplayText(
-        text.substring(0, nextIndex) +
-          (distFromEnd > 0 && distFromEnd < text.length ? "|" : "")
-      );
+    const timeout = setTimeout(
+      () => setDisplayText(getTextEdit(displayText, text)),
+      interval
+    );
+    return () => clearTimeout(timeout);
+  }, [displayText, text, interval]);
+
+  const hasCursor = !(
+    displayText.length === 0 || displayText.length === text.length
+  );
+
+  return displayText + (hasCursor ? "|" : "");
+};
+
+export const usePlaybackSequence = (texts, interval, delay) => {
+  const [textIdx, setTextIdx] = useState(0);
+  const displayText = useTextPlayback(texts[textIdx], interval);
+
+  useEffect(() => {
+    if (displayText === texts[textIdx] && delay !== Infinity) {
       const timeout = setTimeout(
-        () => setNextIndex((current) => current + 1),
-        interval
+        () => setTextIdx((current) => (current + 1) % texts.length),
+        delay
       );
+
       return () => clearTimeout(timeout);
     }
-  }, [text, interval, nextIndex]);
-
-  return displayText;
+  }, [displayText, delay, texts, textIdx]);
+  return displayText || " ";
 };
 
 export default useTextPlayback;
