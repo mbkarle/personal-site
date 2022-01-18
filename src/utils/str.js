84776281e5596ef33f200f47a853211fa208f458
Matthew Karle <mkarle2178@gmail.com>
2022-01-18 13:40:53 -0500

    add programmatic clipboard copy util

--- src/utils/str.js
+++ src/utils/str.js
@@ -1,2 +1,10 @@
 export const capitalize = (str) =>
   str?.charAt(0)?.toUpperCase() + str?.substring(1);
+
+export const copyToClipboard = (text) => {
+  const type = "text/plain";
+  const data = [new ClipboardItem({ [type]: new Blob([text], { type }) })];
+  return navigator?.clipboard?.write(data);
+};
+
+copyToClipboard;
