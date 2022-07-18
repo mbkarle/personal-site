13292e98da2f1e726c38682fea0dce6fb05504ea
Matthew Karle <mkarle2178@gmail.com>
2022-07-17 19:59:21 -0600

    add github markdown plugin

--- src/components/basics/md/index.js
+++ src/components/basics/md/index.js
@@ -5,18 +5,20 @@ import ReactMarkdown from "react-markdown";
 import { CodeBlock } from "react-code-blocks";
 import mergeDefaults from "utils/merge-defaults";
 import GlobalOrLocalLink from "components/basics/global-or-local-link";
+import gfm from "remark-gfm";
 
 const MD = (props) => (
   <ReactMarkdown
+    remarkPlugins={[gfm]}
     components={{
       p: (props) => {
         // default to use divs instead of p
-        return <div {...props} />;
+        return <div className="md-p" {...props} />;
       },
       a: (props) => {
         return <GlobalOrLocalLink className={styles.link} {...props} />;
       },
-      h2: (props) => <div className={styles.customH2} {...props} />,
+      h2: (props) => <div className={`${styles.customH2} md-h2`} {...props} />,
       code: (props) =>
         props.inline ? (
           <code className={styles.code} {...props} />
